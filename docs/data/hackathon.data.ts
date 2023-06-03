9a906e30bf6130efb8787c711355f71008ae907e
LiangShuang <liangshuang@sutu.tech>
2023-06-03 12:06:21 +0800

    add hackathon

--- docs/data/hackathon.data.ts
+++ docs/data/hackathon.data.ts
@@ -1,4 +1,13 @@
 const projectStr=`
+
+- name: Cairo 1.0 Hackathon
+  url: https://taikai.network/pragma-oracle/hackathons/Cairo1hackathon/overview
+  prizes: $80,000
+  date: Jun 11 – July 3, 2023
+  location: Online
+  description: |
+    为了促进 Cairo 1.0 的采用并鼓励新鲜思想加入我们的社区并构建创新应用程序，Pragma Oracle 正在组织为期两周的在线黑客马拉松。
+
 - name: Autonomous Worlds hackathon
   url: https://ethglobal.com/events/autonomous
   prizes: $50,000
